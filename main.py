f9da6b6d35d566945977e8ed1785661ab929b913
Rem Mora <remortalite@gmail.com>
2025-12-24 15:34:16 +0700

    [+] add basic router

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+from titanic.routes import router
+
+
 def main():
     print("Hello from titanic!")
+
+    app = FastAPI()
+
+    app.include_router(router)
+
+    uvicorn.run(app, port=10000)
 
 
 if __name__ == "__main__":
