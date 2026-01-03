bc050d6daffc530cd32e1294f86b599927a55df8
Rem Mora <remortalite@gmail.com>
2026-01-03 21:03:38 +0700

    [*] update tests: wallet imports as a module

--- tests/unit/wallet_test.py
+++ tests/unit/wallet_test.py
@@ -1,11 +1,11 @@
 import pytest
 from titanic import exceptions
 from titanic.models import Wallet
-from titanic.services.wallet import get_wallet, create_wallet, update_wallet, delete_wallet
+from titanic.services import wallet as wallet_service
 
 
 def test_get_wallet():
-    wallet: Wallet = get_wallet(id=0)
+    wallet: Wallet = wallet_service.get_wallet(id=0)
     
     assert wallet is not None
 
@@ -17,13 +17,13 @@ def test_create_wallet():
         'bank': 'sber',
     }
     
-    wallet: Wallet = create_wallet(
+    wallet: Wallet = wallet_service.create_wallet(
         user=data['user'],
         name=data['name'],
         bank=data['bank'],
     )
 
-    wallet_created = get_wallet(id=wallet.id)
+    wallet_created = wallet_service.get_wallet(id=wallet.id)
 
     assert wallet_created.user == wallet.user
     assert wallet_created.name == wallet.name
@@ -38,7 +38,7 @@ def wallet_obj():
         'bank': 'sber',
     }
     
-    wallet: Wallet = create_wallet(
+    wallet: Wallet = wallet_service.create_wallet(
         user=data['user'],
         name=data['name'],
         bank=data['bank'],
@@ -53,22 +53,22 @@ def test_update_wallet(wallet_obj):
         'bank': 'Tbank',
     }
     
-    update_wallet(wallet_obj, name=new_data['name'])
+    wallet_service.update_wallet(wallet_obj, name=new_data['name'])
 
-    wallet_new = get_wallet(wallet_obj.id)
+    wallet_new = wallet_service.get_wallet(wallet_obj.id)
     assert wallet_new.name == new_data['name']
     assert not wallet_new.bank == new_data['bank']
     
-    update_wallet(wallet_new, bank=new_data['bank'])
+    wallet_service.update_wallet(wallet_new, bank=new_data['bank'])
     
-    wallet_new_new = get_wallet(wallet_obj.id)
+    wallet_new_new = wallet_service.get_wallet(wallet_obj.id)
     assert wallet_new.name == new_data['name']
     assert wallet_new_new.bank == new_data['bank']
     
 
 def test_delete_wallet(wallet_obj):
     wallet_id = wallet_obj.id
-    delete_wallet(wallet_obj)
+    wallet_service.delete_wallet(wallet_obj)
     
     with pytest.raises(exceptions.WalletNotExists):
-        assert get_wallet(id=wallet_id)
+        assert wallet_service.get_wallet(id=wallet_id)
